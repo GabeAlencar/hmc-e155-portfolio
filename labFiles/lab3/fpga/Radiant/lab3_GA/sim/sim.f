869681ddad467d9db3b35eba465161f6a36a9ae0
Gabe Alencar <gmenendezdealencar@g.hmc.edu>
2026-09-21 16:11:25 -0700

    lab3

---
+++ labFiles/lab3/fpga/Radiant/lab3_GA/sim/sim.f
@@ -0,0 +1,34 @@
+-L work
+-reflib pmi_work
+-reflib ovi_ice40up
+
+
+"C:/Users/gmenendezdealencar/Documents/GitHub/hmc-e155-portfolio/labFiles/lab3/fpga/Radiant/lab3_GA/source/impl_1/lab3_GA.sv" 
+"C:/Users/gmenendezdealencar/Documents/GitHub/hmc-e155-portfolio/labFiles/lab3/fpga/Radiant/lab3_GA/source/impl_1/synchronizer.sv" 
+"C:/Users/gmenendezdealencar/Documents/GitHub/hmc-e155-portfolio/labFiles/lab3/fpga/Radiant/lab3_GA/source/impl_1/scanner.sv" 
+"C:/Users/gmenendezdealencar/Documents/GitHub/hmc-e155-portfolio/labFiles/lab3/fpga/Radiant/lab3_GA/source/impl_1/key_decoder.sv" 
+"C:/Users/gmenendezdealencar/Documents/GitHub/hmc-e155-portfolio/labFiles/lab3/fpga/Radiant/lab3_GA/source/impl_1/key_control.sv" 
+"C:/Users/gmenendezdealencar/Documents/GitHub/hmc-e155-portfolio/labFiles/lab3/fpga/Radiant/lab3_GA/source/impl_1/display_mux.sv" 
+"C:/Users/gmenendezdealencar/Documents/GitHub/hmc-e155-portfolio/labFiles/lab3/fpga/Radiant/lab3_GA/source/impl_1/digit_reg.sv" 
+"C:/Users/gmenendezdealencar/Documents/GitHub/hmc-e155-portfolio/labFiles/lab3/fpga/Radiant/lab3_GA/source/impl_1/debouncer.sv" 
+"C:/Users/gmenendezdealencar/Documents/GitHub/hmc-e155-portfolio/labFiles/lab3/fpga/Radiant/lab3_GA/source/impl_1/counter.sv" 
+"C:/Users/gmenendezdealencar/Documents/GitHub/hmc-e155-portfolio/labFiles/lab3/fpga/Radiant/lab3_GA/source/impl_1/sevenseg_decoder.sv" 
+"C:/Users/gmenendezdealencar/Downloads/display_mux_tb.sv" 
+-sv
+-optionset VOPTDEBUG
++noacc+pmi_work.*
++noacc+ovi_ice40up.*
+
+-vopt.options
+  -suppress vopt-7033
+-end
+
+-gui
+-top display_mux_tb
+-vsim.options
+  -suppress vsim-7033,vsim-8630,3009,3389
+-end
+
+-do "view wave"
+-do "add wave /*"
+-do "run 100 ns"
